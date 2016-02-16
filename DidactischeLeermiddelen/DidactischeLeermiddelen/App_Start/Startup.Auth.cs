8df32cf312b2eeb5f2b2b2aea2df5a95695872c3
Ward Vanlerberghe <ward.vanlerberghe.i2568@student.hogent.be>
2016-02-16 22:25:15 +0100

    Revert to UC1 incl build

--- DidactischeLeermiddelen/DidactischeLeermiddelen/App_Start/Startup.Auth.cs
+++ DidactischeLeermiddelen/DidactischeLeermiddelen/App_Start/Startup.Auth.cs
@@ -17,7 +17,7 @@ namespace DidactischeLeermiddelen
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
+            // Configure the db context, user manager and signin manager to use a single instance per request 
             app.CreatePerOwinContext(LeermiddelenContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
