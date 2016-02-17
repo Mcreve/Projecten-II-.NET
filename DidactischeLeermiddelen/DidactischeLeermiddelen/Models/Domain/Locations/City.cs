a472fa6ce61cd7500c35276cbafaffe21dee42d1
Ward Vanlerberghe <ward.vanlerberghe.i2568@student.hogent.be>
2016-02-17 09:45:58 +0100

    Revert "Revert "Revert "#6 Ontwerp"""
    This reverts commit 9fab767569b4a2a4ae51a59f52010b3c26a74305.

--- DidactischeLeermiddelen/DidactischeLeermiddelen/Models/Domain/Locations/City.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DidactischeLeermiddelen.Models.Domain.Locations
-{
-    public class City
-    {
-        public string PostalCode { get; set; }
-        public string Name { get; set; }
-    }
-}
