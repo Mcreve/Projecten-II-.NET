cd045d8a8f5e9f4bcd8cce0a64e34560787af187
Ward Vanlerberghe <ward.vanlerberghe.i2568@student.hogent.be>
2016-03-01 20:23:47 +0100

    Aanpassing routeconfig en redirect na login

--- DidactischeLeermiddelen/DidactischeLeermiddelen/Controllers/AccountController.cs
+++ DidactischeLeermiddelen/DidactischeLeermiddelen/Controllers/AccountController.cs
@@ -164,7 +164,7 @@ namespace DidactischeLeermiddelen.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Catalog");
                 }
                 AddErrors(result);
             }
@@ -393,7 +393,7 @@ namespace DidactischeLeermiddelen.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Catalog");
         }
 
         //
@@ -450,7 +450,7 @@ namespace DidactischeLeermiddelen.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Catalog");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
