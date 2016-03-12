ea4827d30e083bd910187a56c6ddd1bd709097df
Ward Vanlerberghe <ward.vanlerberghe.i2568@student.hogent.be>
2016-03-13 00:33:46 +0100

    Vertaling kalender

--- DidactischeLeermiddelen/DidactischeLeermiddelen/Scripts/CustomScripts/WeekPicker.js
+++ DidactischeLeermiddelen/DidactischeLeermiddelen/Scripts/CustomScripts/WeekPicker.js
@@ -1,14 +1,26 @@
 ﻿var startDate,
         endDate;
 
-
+$.fn.datepicker.dates['nl'] = {
+    days: ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"],
+    daysShort: ["Zo", "Ma", "Di", "Wo", "Do", "Vr", "Za"],
+    daysMin: ["Zo", "Ma", "Di", "Wo", "Do", "Vr", "Za"],
+    months: ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "July", "Augustus", "September", "October", "November", "December"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    today: "Vandaag",
+    clear: "Reset",
+    format: "dd/mm/yyyy",
+    titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
+    weekStart: 1
+};
 
 $('#weekpicker').datepicker({
     autoclose: true,
     format: 'dd/mm/yyyy',
     forceParse: false,
     startDate: new Date(),
-    weekStart: 1
+    weekStart: 1,
+    language: "nl"
 }).on("changeDate", function (e) {
     var date = e.date;
     startDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay()+1);
