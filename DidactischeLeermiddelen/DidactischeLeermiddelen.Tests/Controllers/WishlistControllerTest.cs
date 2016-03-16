37e676dad17860c1b90ac326ac935b11df7eb41e
Maxim Crève <maxim.creve.r7590@student.hogent.be>
2016-03-16 13:23:26 +0100

    Wishlist testen werken weer.

--- DidactischeLeermiddelen/DidactischeLeermiddelen.Tests/Controllers/WishlistControllerTest.cs
+++ DidactischeLeermiddelen/DidactischeLeermiddelen.Tests/Controllers/WishlistControllerTest.cs
@@ -6,30 +6,49 @@ using DidactischeLeermiddelen.Models.Domain;
 using DidactischeLeermiddelen.Models.Domain.Users;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Web;
+using System.Web.Routing;
 
 namespace DidactischeLeermiddelen.Tests.Controllers
 {
     [TestClass]
     public class WishlistControllerTest
     {
-        private Wishlist wishlist;
+
         private WishlistController wishlistController;
         private DummyDataContext context;
         private Mock<ILearningUtilityRepository> itemRepository;
+        private Mock<IUserRepository> userRepository;
+        private User student;
+        private User lector;
+        private Wishlist wishlist;
+        private Mock<HttpRequestBase> request;
+        private Mock<HttpContextBase> httpcontext;
 
         [TestInitialize]
         public void TestInitialize()
         {
             //Arrange
+            request = new Mock<HttpRequestBase>();
+            httpcontext = new Mock<HttpContextBase>();
+            httpcontext.SetupGet(x => x.Request).Returns(request.Object);
+            request.SetupGet(x => x.Headers).Returns(new System.Net.WebHeaderCollection { { "X-Requested-With", "" } });
+            student = UserFactory.CreateUserWithUserType(UserType.Student);
+            lector = UserFactory.CreateUserWithUserType(UserType.Lector);
             context = new DummyDataContext();
             itemRepository = new Mock<ILearningUtilityRepository>();
             itemRepository.Setup(i => i.FindAll()).Returns(context.LearningUtilityList);
             itemRepository.Setup(i => i.FindBy(1)).Returns(context.LearningUtility1);
             itemRepository.Setup(i => i.FindBy(2)).Returns(context.LearningUtility2);
             itemRepository.Setup(i => i.FindBy(3)).Returns(context.LearningUtility3);
-            wishlistController = new WishlistController(itemRepository.Object);
+            userRepository = new Mock<IUserRepository>();
+            userRepository.Setup(i => i.FindBy("Benjamin.vertonghen@student.hogent.be")).Returns(context.Student1);
+            wishlistController = new WishlistController(itemRepository.Object, userRepository.Object);
+            wishlistController.ControllerContext = new ControllerContext(httpcontext.Object, new RouteData(), wishlistController);
             wishlist = new Wishlist();
             wishlist.AddItem(context.LearningUtility1);
+            student.Wishlist = wishlist;
+
         }
 
         #region Index
@@ -38,9 +57,11 @@ namespace DidactischeLeermiddelen.Tests.Controllers
         {
             //Arrange
             Wishlist emptyWishlist = new Wishlist();
+            student.Wishlist = emptyWishlist;
+
 
             //Act
-            ViewResult result = wishlistController.Index(emptyWishlist) as ViewResult;
+            ViewResult result = wishlistController.Index(student) as ViewResult;
 
             //Assert
             Assert.IsNotNull(result);
@@ -51,7 +72,7 @@ namespace DidactischeLeermiddelen.Tests.Controllers
         public void IndexShowsContentsOfWishlistWhenWishlistNotEmpty()
         {
             //Act
-            ViewResult result = wishlistController.Index(wishlist) as ViewResult;
+            ViewResult result = wishlistController.Index(student) as ViewResult;
 
             //Assert
             Assert.IsNotNull(result);
@@ -62,10 +83,10 @@ namespace DidactischeLeermiddelen.Tests.Controllers
         public void IndexShowsWishlistWhenWishlistNotEmpty()
         {
             //Act
-            ViewResult result = wishlistController.Index(wishlist) as ViewResult;
-            
+            ViewResult result = wishlistController.Index(student) as ViewResult;
+
             //Assert
-            Assert.AreEqual(1, wishlist.NumberOfItems);
+            Assert.AreEqual(1, student.Wishlist.NumberOfItems);
         }
         #endregion
 
@@ -74,7 +95,7 @@ namespace DidactischeLeermiddelen.Tests.Controllers
         public void AddRedirectsToCatalog()
         {
             //Act
-            RedirectToRouteResult result = wishlistController.Add(3, wishlist) as RedirectToRouteResult;
+            RedirectToRouteResult result = wishlistController.Add(3, student) as RedirectToRouteResult;
 
             //Assert
             Assert.IsNotNull(result);
@@ -87,10 +108,10 @@ namespace DidactischeLeermiddelen.Tests.Controllers
         public void AddWillAddItemToWishlist()
         {
             //Act
-            RedirectToRouteResult result = wishlistController.Add(3, wishlist) as RedirectToRouteResult;
+            RedirectToRouteResult result = wishlistController.Add(3, student) as RedirectToRouteResult;
 
             //Assert
-            Assert.AreEqual(2, wishlist.NumberOfItems);
+            Assert.AreEqual(2, student.Wishlist.NumberOfItems);
             itemRepository.Verify(l => l.FindBy(3), Times.Once);
         }
         #endregion
@@ -100,7 +121,7 @@ namespace DidactischeLeermiddelen.Tests.Controllers
         public void RemoveRedirectsToIndex()
         {
             //Act
-            RedirectToRouteResult result = wishlistController.Remove(1, wishlist) as RedirectToRouteResult;
+            RedirectToRouteResult result = wishlistController.Remove(1, student) as RedirectToRouteResult;
 
             //Assert
             Assert.IsNotNull(result);
@@ -112,12 +133,26 @@ namespace DidactischeLeermiddelen.Tests.Controllers
         public void RemoveWillRemoveItemFromWishlist()
         {
             //Act
-            RedirectToRouteResult result = wishlistController.Remove(1, wishlist) as RedirectToRouteResult;
+            RedirectToRouteResult result = wishlistController.Remove(1, student) as RedirectToRouteResult;
 
             //Assert
-            Assert.AreEqual(0, wishlist.NumberOfItems);
+            Assert.AreEqual(0, student.Wishlist.NumberOfItems);
             itemRepository.Verify(l => l.FindBy(1), Times.Once);
         }
+
+        public void ReturnPartialViewIfIsAjaxRequest()
+        {
+            //Arrange
+            
+            request.SetupGet(x => x.Headers).Returns(new System.Net.WebHeaderCollection { { "X-Requested-With", "XMLHttpRequest" } });
+
+            //Act
+            PartialViewResult result = wishlistController.Index(student) as PartialViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
 
         #endregion
     }
